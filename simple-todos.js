ff75504488911c61db89f9e36fedcddbe9edaf85
Brent Daniel <brent.daniel@pnnl.gov>
2015-05-01 14:29:06 -0600

    Ability to delete and check off tasks.

--- simple-todos.js
+++ simple-todos.js
@@ -13,20 +13,29 @@ if (Meteor.isClient) {
 
   Template.body.events({
   "submit .new-task": function (event) {
-    // This function is called when the new task form is submitted
+      // This function is called when the new task form is submitted
 
-    var text = event.target.text.value;
+      var text = event.target.text.value;
 
-    Tasks.insert({
-      text: text,
-      createdAt: new Date() // current time
-    });
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
 
-    // Clear form
-    event.target.text.value = "";
+      // Clear form
+      event.target.text.value = "";
 
-    // Prevent default form submit
-    return false;
-  }
-});
+      // Prevent default form submit
+      return false;
+    }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
+  });
 }
